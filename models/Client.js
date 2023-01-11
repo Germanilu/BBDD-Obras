2509dc5f724811003cc1026d20f29873a1273784
Luciano Germani <lucianogermani93@gmail.com>
2023-01-11 13:09:00 +0100

    Fix: Client Schema adding password and unique email

--- models/Client.js
+++ models/Client.js
@@ -24,6 +24,12 @@ const ClientSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
     },
     role: {
         type: String,
