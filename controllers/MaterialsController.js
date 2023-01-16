565a2c2da1e3316f176fb50fcba8f5701e36a472
Luciano Germani <lucianogermani93@gmail.com>
2023-01-16 15:50:02 +0100

    Feat: Create get all materials by project id

--- controllers/MaterialsController.js
+++ controllers/MaterialsController.js
@@ -56,4 +56,46 @@ materialsController.create = async(req,res) => {
     }
 }
 
+materialsController.getAllMaterialInProject = async(req,res) => {
+    try {
+        const projectId = req.params.id
+        const userId = req.user_id
+
+        //Search the project by ID in DB and check if requester is the actual project manager, if not, throw error
+        const project = await Project.find({_id: projectId})
+        project.map(e => {
+            const ProjectManagerID = e.projectManagerId.toString()
+ 
+            if(userId !== ProjectManagerID){
+                return res.status(500).json({
+                    success: false,
+                    message: "No tienes permisos para acceder a los materiales de este proyecto"
+                })
+            }
+        })
+
+        //Find material by projectId
+        const materiales = await Materials.find({projectId : projectId})
+        return res.status(200).json({
+            success: true,
+            message: "Estos son los materiales para este proyecto!",
+            data: materiales
+        })
+
+    } catch (error) {
+        if (error?.message.includes('Cast to ObjectId failed')) {
+            return res.status(404).json({
+                    success: true,
+                    messagge: "No se pueden encontrar materiales en este proyecto"
+
+                });
+        }
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to search for Material ',
+            error: error.message
+        })
+    }
+}
+
 module.exports = materialsController;
--- routes/materials.routes.js
+++ routes/materials.routes.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 
 //Routes
 router.post('/addMaterial/:id', verifyToken, materialsController.create);
+router.get('/projectMaterial/:id', verifyToken, materialsController.getAllMaterialInProject);
 
 module.exports = router;
