7315fa3a5e0e817839ab6f1b80ca344c6be536ff
Luciano Germani <lucianogermani93@gmail.com>
2023-01-16 16:17:09 +0100

    Feat: Create update material by id

--- controllers/MaterialsController.js
+++ controllers/MaterialsController.js
@@ -3,7 +3,7 @@ const Materials = require('../models/Materials')
 
 const materialsController = {};
 
-
+//Create new material
 materialsController.create = async(req,res) => {
     try {
         const projectId = req.params.id
@@ -56,6 +56,7 @@ materialsController.create = async(req,res) => {
     }
 }
 
+//Get all materials use in the project
 materialsController.getAllMaterialInProject = async(req,res) => {
     try {
         const projectId = req.params.id
@@ -93,6 +94,95 @@ materialsController.getAllMaterialInProject = async(req,res) => {
         return res.status(500).json({
             success: false,
             message: 'Unable to search for Material ',
+            error: error.message
+        })
+    }
+}
+
+//Get one material by his ID
+
+materialsController.getMaterialByID = async(req,res) => {
+    try {
+        const materialId = req.params.id;
+        const userId = req.user_id;
+
+        //Search the material by ID in DB and check if requester is the actual project manager, if not, throw error
+        const material = await Materials.find({_id: materialId})
+        console.log(material)
+
+        material.map(e => {
+            const ProjectManagerID = e.projectManagerId.toString();
+            if(userId !== ProjectManagerID){
+                return res.status(500).json({
+                    success: false,
+                    message: "No tienes permisos para acceder a este material"
+                })
+            }
+        })
+
+        return res.status(200).json({
+            success:true,
+            message: "Aqui tienes el material que buscabas",
+            data: material
+        })
+
+
+    } catch (error) {
+        if (error?.message.includes('Cast to ObjectId failed')) {
+            return res.status(404).json({
+                    success: true,
+                    messagge: "No se pueden encontrar el material en este proyecto"
+
+                });
+        }
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to search for Material ',
+            error: error.message
+        })
+    }
+}
+
+
+materialsController.updateMaterial = async(req,res) => {
+    try {
+        const materialId = req.params.id;
+        const userId = req.user_id;
+        const {name, quantity} = req.body;
+
+        //search for material and throw error if requester is not the PM material ID
+        const material = await Materials.find({_id:materialId})
+        material.map(e => {
+            const ProjectManagerID = e.projectManagerId.toString();
+            if(userId !== ProjectManagerID){
+                return res.status(500).json({
+                    success: false,
+                    message: "No tienes permisos para modificar este material"
+                })
+            }
+            //Editing the material properties
+            e.name = name || e.name;
+            e.quantity = quantity || e.quantity;
+        })
+        //Saving new material and resolve
+        await material[0].save()
+        return res.status(200).json({
+            success: true,
+            message: "Material actualizado con exito",
+            data: material
+        })
+
+    } catch (error) {
+        if (error?.message.includes('Cast to ObjectId failed')) {
+            return res.status(404).json({
+                    success: true,
+                    messagge: "No se puede actualizar los datos del material"
+
+                });
+        }
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to update the Material ',
             error: error.message
         })
     }
--- routes/materials.routes.js
+++ routes/materials.routes.js
@@ -5,5 +5,7 @@ const router = require('express').Router();
 //Routes
 router.post('/addMaterial/:id', verifyToken, materialsController.create);
 router.get('/projectMaterial/:id', verifyToken, materialsController.getAllMaterialInProject);
+router.get('/material/:id', verifyToken, materialsController.getMaterialByID);
+router.put('/updateMaterial/:id', verifyToken, materialsController.updateMaterial);
 
 module.exports = router;
