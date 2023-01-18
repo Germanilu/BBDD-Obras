5a16d1291585494acc0304ec5bf99c1c6cabfc6a
Germanilu <lucianogermani93@gmail.com>
2023-01-18 13:28:19 +0100

    Fix: Implement checkRole middleware in projectController

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,12 +1,13 @@
 const projectController = require('../controllers/ProjectController');
 const verifyToken = require('../middlewares/verifyToken');
+const checkRole = require('../middlewares/checkRole')
 const router = require('express').Router();
 
 //Routes
-router.post('/newProject/:id', verifyToken, projectController.create);
-router.get('/myProjects', verifyToken, projectController.getMyProject);
-router.get('/ongoingProject', verifyToken, projectController.ongoingProject);
-router.get('/endedProject', verifyToken, projectController.endedProjects);
+router.post('/newProject/:id', verifyToken,checkRole, projectController.create);
+router.get('/myProjects', verifyToken,checkRole, projectController.getMyProject);
+router.get('/ongoingProject', verifyToken,checkRole, projectController.ongoingProject);
+router.get('/endedProject', verifyToken,checkRole, projectController.endedProjects);
 router.put('/completeProject/:id', verifyToken, projectController.completeProject)
 router.delete('/deleteProject/:id', verifyToken, projectController.delete)
 module.exports = router;
