a0e90e19a3d18494fcba17fbd67997a1059c3555
Germanilu <lucianogermani93@gmail.com>
2023-01-18 13:33:19 +0100

    Fix: Add checkRole middleware in messageController

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,9 +1,10 @@
 const messageController = require('../controllers/MessageController');
 const verifyToken = require('../middlewares/verifyToken');
+const checkRole = require('../middlewares/checkRole')
 const router = require('express').Router();
 
 //Routes
-router.post('/newMessage/:id', verifyToken, messageController.create);
+router.post('/newMessage/:id', verifyToken,checkRole, messageController.create);
 router.get('/messages/:id',verifyToken,messageController.getAllMessage);
 router.delete('/message/:id',verifyToken,messageController.delete);
 router.put('/message/:id',verifyToken,messageController.update);
